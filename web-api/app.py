22ae14a44b1d0287e330fdb1dc492cd1f05fa0f3
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-07-16 00:18:30 -0400

    small edit

--- web-api/app.py
+++ web-api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Sample Web API"
+    return "Welcome to the Company Y API"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
